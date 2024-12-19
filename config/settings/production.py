a1b98ebc6deda17028111e6c57d79ae5f1e3f960
Johannes Mario Meissner <jmariomeissner@gmail.com>
2024-12-19 21:30:08 +0900

    Test

--- config/settings/production.py
+++ config/settings/production.py
@@ -18,7 +18,13 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=[
+        env.str("RAILWAY_PUBLIC_DOMAIN"),
+    ],
+)
+
 
 # DATABASES
 # ------------------------------------------------------------------------------
