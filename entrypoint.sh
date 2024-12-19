4e7a0caff247e7dc9dc32f414f60d1cc4e2672c6
Johannes Mario Meissner <jmariomeissner@gmail.com>
2024-12-19 22:14:36 +0900

    Add compress call

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,11 @@ uv run python manage.py migrate --noinput
 # Collect static files
 uv run python manage.py collectstatic --noinput
 
+# Run compress
+uv run python manage.py compress
+
 # Run check
 uv run python manage.py check --deploy
-
 
 # Start Gunicorn
 uv run gunicorn config.wsgi
